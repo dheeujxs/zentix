2e0437463b8bfa38b2971ad2317898c99126aaab
Dheeraj <goswamidheeraj760gmail.com>
2025-07-24 19:02:34 +0530

    18:agent memory

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,9 @@
 import { inngest } from "./client";
 import {Sandbox} from '@e2b/code-interpreter'
-import {  gemini ,  createAgent, createTool, createNetwork, type Tool } from "@inngest/agent-kit";
+import {  gemini ,  createAgent, createTool, createNetwork, type Tool, Message, createState } from "@inngest/agent-kit";
 import { getSandbox, lastAssistantTextMessageContent } from "./utlis";
 import {  z } from "zod";
-import { PROMPT } from "@/prompts";
+import { FRAGMENT_TITLE_PROMPT, PROMPT, RESPONSE_PROMPT } from "@/prompts";
 import { prisma } from "@/lib/db";
 
 
@@ -22,6 +22,41 @@ export const codeAgentFunction = inngest.createFunction(
     const sandbox = await  Sandbox.create("zentix-nextjs-test");
       return sandbox.sandboxId;
      });
+
+     const previousMessages = await step.run("get-previous-messages", async () =>{
+      const formattedMessages : Message[] = [];
+
+      const messages = await prisma.message.findMany({
+        where:{
+          projectId: event.data.projectedId
+        },
+        orderBy:{
+          createdAt:"desc"
+        }
+      })
+      for(const message of messages) {
+        formattedMessages.push({
+          type:"text",
+          role:message.role === "ASSISTANT" ? 'assistant':"user",
+          content:message.content
+        })
+      }
+      return formattedMessages;
+     })
+
+     const state  = createState<AgentState>(
+      {
+        summary:"",
+        files:{},
+      },
+      {
+        messages:previousMessages,
+      }
+     )
+
+     
+
+
  const codeAgent = createAgent<AgentState>({
       name: "code-agent",
       description:"An expert coding agent",
@@ -35,6 +70,7 @@ export const codeAgentFunction = inngest.createFunction(
           parameters:z.object({
             command:z.string(),
           }),
+
           handler:async({command},{step}) => {
             return await step?.run("terminal", async() =>{
               const buffers = {stdout:"",stderr:""};
@@ -145,6 +181,7 @@ export const codeAgentFunction = inngest.createFunction(
       name:"coding-agent-netwrok",
       agents:[codeAgent],
       maxIter:15,
+      defaultState: state,
       router:async ({network}) =>{
         const summary = network.state.data.summary;
 
@@ -156,7 +193,55 @@ export const codeAgentFunction = inngest.createFunction(
       }
     })
 
-      const result = await network.run(event.data.value);
+      const result = await network.run(event.data.value , {state});
+      const fragmentTitleGenerator = createAgent({
+        name:"fragment-title-generator",
+        description:"A Fragmetn title generator",
+        system:FRAGMENT_TITLE_PROMPT,
+        model: gemini({ model: "gemini-2.0-flash",
+      }),
+        
+      })
+      const ResponseGenerator = createAgent({
+        name:"response-generator",
+        description:"A Fragmetn title generator",
+        system:RESPONSE_PROMPT,
+        model: gemini({ model: "gemini-2.0-flash",
+      }),
+        
+      })
+
+      const {output:fragmentTitleOutput} = await fragmentTitleGenerator.run(result.state.data.summary)
+      const {output:responseOutput} = await ResponseGenerator.run(result.state.data.summary)
+
+      const generateFragmentTitle= () =>{
+        if((fragmentTitleOutput[0].type) !=="text"){
+            return "Fragment"
+
+        }
+        if(Array.isArray(fragmentTitleOutput[0].content)){
+          return fragmentTitleOutput[0].content.map((txt) => txt).join("")
+        }
+
+        else {
+          return fragmentTitleOutput[0].content
+        }
+
+      }
+      const generateResponse= () =>{
+        if((responseOutput[0].type) !=="text"){
+            return "Here you go"
+
+        }
+        if(Array.isArray(responseOutput[0].content)){
+          return responseOutput[0].content.map((txt) => txt).join("")
+        }
+
+        else {
+          return responseOutput[0].content
+        }
+
+      }
 
       const isError = 
       !result.state.data.summary ||
@@ -186,7 +271,7 @@ await step.run("save-result", async () => {
       projectId:event.data.projectId,
       role: "ASSISTANT",
       type: "RESULT",
-      content: result.state.data.summary ?? "No summary generated",
+      content:generateResponse(),
     },
   });
 
@@ -194,7 +279,7 @@ await step.run("save-result", async () => {
     data: {
       messageId: message.id,
       sandboxUrl: sandboxUrl,
-      title: "Fragment",
+      title: generateFragmentTitle(),
       files: result.state.data.files,
     },
   });
